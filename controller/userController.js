3b13e8545752487c245a10867f764000cb4497a4
Gankhuu Gereltkhuu <HP230720@G303-CC0130.local>
2024-02-01 16:54:03 +0800

    login

--- controller/userController.js
+++ controller/userController.js
@@ -1,11 +1,30 @@
 const fs = require('fs')
 const userModel = require("../models/userModel.js")
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
+
+const loginUser = async (req, res) => {
+    try {
+        const body = req.body;
+        const user = await userModel.findOne({ email: body.email });
+
+        if (user) {
+            res.status(200).send(user._id);
+        } else {
+            res.status(404).send("Not Found");
+        }
+    } catch (error) {
+        console.error("Error during login:", error);
+        res.status(500).send("Internal Server Error");
+    }
+};
 
 const signUpUser = async (req, res) => {
+    const body = req.body;
+    const password = body.password;
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    const data = {...body, password: hashedPassword };
     try {      
-        const userData = req.body;
-        const user = await userModel.create(userData);
+        const user = await userModel.create(data);
         res.status(200).send({ id: user._id });
     } catch (error) {
         console.error(error);
@@ -13,4 +32,4 @@ const signUpUser = async (req, res) => {
     }
 }
 
-module.exports = { signUpUser };
+module.exports = { signUpUser, loginUser };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,41 @@
 const { Router } = require('express');
-const userModel = require('../models/userModel.js');
-const { signUpUser } = require('../controller/userController.js');
-
 const userRouter = Router();
+const userModel = require('../models/userModel.js');
+const { signUpUser, loginUser } = require('../controller/userController.js');
+const bcrypt = require("bcrypt");
 
-userRouter.post('/signUp', signUpUser);
+const validateEmailAddress = async (req, res, next) =>{ 
+    const body = req.body;
+    const user = await userModel.findOne({email: body.email});
+    if(!user) {
+        next();
+    } else {
+        res.status(403).send("Email Address is already used")
+    }
+} 
+
+const validatePasswordAndEmailAddress = async (req, res, next) => {
+    const body = req.body;
+    try {
+        const user = await  userModel.findOne({ email: body.email });
+        console.log(user)
+        if (user) {
+            const isPasswordCorrect = bcrypt.compareSync(body.password, user.password);
+            if (isPasswordCorrect) {
+                next();
+            } else {
+                res.status(401).send("Invalid password");
+            }
+        } else {
+            res.status(401).send("Invalid password");
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
+};
+
+userRouter.post('/login', validatePasswordAndEmailAddress, loginUser )
+userRouter.post('/signUp', validateEmailAddress, signUpUser);
 
 module.exports = userRouter;
